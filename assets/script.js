e18f929e13f8917a8465e854cda81d6cbe14ac01
ColleryCorentin <corentin.collery@epitech.eu>
2022-07-22 19:08:31 +0200

    ✅ Update cli and add function contact, cv, about, education, wget

--- assets/script.js
+++ assets/script.js
@@ -76,7 +76,7 @@ function script(spt) {
             case 'cat ABOUT':
             case 'Cat ABOUT':
             case 'CAT ABOUT':
-                about_input()
+                about_input(res)
                 break
 
             case 'ls skills':
@@ -84,6 +84,23 @@ function script(spt) {
             case 'skills':
             case 'skill':
                 skills_input(res)
+                break
+
+            case 'ls education':
+            case 'ls éducation':
+            case 'education':
+            case 'éducation':
+                education_input(res)
+                break
+
+            case 'wget cv':
+            case 'WGET CV':
+                wget_input(res)
+                break
+
+            case 'cat contact':
+            case 'contact':
+                contact_input(res)
                 break
 
             case 'clear':
@@ -108,6 +125,7 @@ function command_not_find(res) {
     var p = document.createElement("p")
     newDiv.append(p_res)
     newDiv.append(p)
+    p_res.innerText = "$ " + res
     p.innerText = "$ Command not found : " + res
     content_div.appendChild(newDiv);
 }
@@ -123,7 +141,7 @@ function help_input(res) {
     newDiv.append(p_res)
     newDiv.append(p)
     p_res.innerText = "$ " + res
-    p.innerText = "$ Commandes valides : --help, cat about, ls skills"
+    p.innerText = "$ Commandes valides : --help, cat about, ,ls competence, ls projets, ls education, wget cv, clear"
     content_div.appendChild(newDiv);
 }
 
@@ -138,7 +156,7 @@ function skills_input(res) {
     newDiv.append(p_res)
     newDiv.append(p)
     p_res.innerText = "$ " + res
-    p.innerText = "$ Commandes valides : --help, cat about, ls skills"
+    p.innerText = "$ Commandes valides : --help, cat about, ,ls competence, ls projets, ls education, wget cv, clear"
     content_div.appendChild(newDiv);
 }
 
@@ -148,6 +166,77 @@ function clear() {
     // crée un nouvel élément div
 }
 
-function about_input() {
+function about_input(res) {
+    input_term.value = ""
+    const content_div = document.querySelector(".content_input")
+    // crée un nouvel élément div
+    var newDiv = document.createElement("div");
+    // et lui donne un peu de contenu
+    var p_res = document.createElement("p")
+    var p = document.createElement("p")
+    p.classList.add('p_about');
+    newDiv.append(p_res)
+    newDiv.append(p)
+    p_res.innerText = "$ " + res
+    p.innerText = "> Suite à 3 ans d'expérience dans le tourisme, j'ai décidé de me réorienter vers un domaine tout nouveau pour moi : le développement web." +
+        "\n" +
+        "> J'ai 23 ans et je suis actuellement étudiant à Epitech." +
+        "\n" +
+        "> Je suis à la recherche d'une alternance pour ma deuxième année de formation" +
+        "\n" +
+        "> J'ai découvert le monde du développement web en 2020 à la suite de mon bts\n"
+    content_div.appendChild(newDiv);
+}
 
+function education_input(res) {
+    input_term.value = ""
+    const content_div = document.querySelector(".content_input")
+    // crée un nouvel élément div
+    var newDiv = document.createElement("div");
+    // et lui donne un peu de contenu
+    var p_res = document.createElement("p")
+    var p = document.createElement("p")
+    p.classList.add('p_about');
+    newDiv.append(p_res)
+    newDiv.append(p)
+    p_res.innerText = "$ " + res
+    p.innerText = "> Baccalauréat STMG Albert Chatelet, Saint-Pol-sur-Ternoise, France\n" +
+        "> BTS Tourisme - option Multimédia Gaston Berger, Lille, France\n" +
+        "> Initiation Coding B-Tech, Euratechnologies Lille\n" +
+        "> Formation développeur/intégrateur web Epitech, Lille, France\n"
+    content_div.appendChild(newDiv);
+}
+
+
+function wget_input(res) {
+    input_term.value = ""
+    const content_div = document.querySelector(".content_input")
+    // crée un nouvel élément div
+    var newDiv = document.createElement("div");
+    // et lui donne un peu de contenu
+    var p_res = document.createElement("p")
+    newDiv.append(p_res)
+    p_res.innerText = "$ " + res
+    window.open('../assets/cv.pdf', '_blank');
+    content_div.appendChild(newDiv);
+}
+
+function contact_input(res) {
+    input_term.value = ""
+    const content_div = document.querySelector(".content_input")
+    // crée un nouvel élément div
+    var newDiv = document.createElement("div");
+    // et lui donne un peu de contenu
+    var p_res = document.createElement("p")
+
+    var a = document.createElement("a")
+
+    a.setAttribute('href', 'mailto:corentin.collery@epitech.eu')
+    a.classList.add('p_about');
+
+    newDiv.append(p_res)
+    newDiv.append(a)
+    p_res.innerText = "$ " + res
+    a.innerText = "> collery.corentin@epitech.eu"
+    content_div.appendChild(newDiv);
 }
